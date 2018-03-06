316c42775b59640fba8f70b2c10c30b416b37f68
Muhammad Hussain <mnh78614@gmail.com>
2018-03-06 11:52:43 -0800

    Finished wrangling the data and the UI.R/Server.R section...

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-# Luxi Zheng
+# Muhammad Hussain
 
 library("shiny")
 
@@ -39,37 +39,86 @@ latest.finished.season.id <- latest.finished.season.id[1, 1]
 
 base.uri <- "https://www.haloapi.com/stats/h5/player-leaderboards/csr/"
 # resource {seasonId}/{playlistId}
-resource.uri <- paste0(latest.finished.season.id, "/c98949ae-60a8-43dc-85d7-0feb0b92e719")
+resource.uri <- paste0(latest.finished.season.id, "/c98949ae-60a8-43dc-85d7-0feb0b92e719?count=250")
 uri <- paste0(base.uri, resource.uri)
 response <- GET(uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
 response <- fromJSON(content(response, "text"))
-leaders <- response$Results
+leaders <- response$Results %>% flatten()
 
-#call to get the matchID for the players
-base.uri <- "https://www.haloapi.com/stats/h5/players/"
-#player}/matches[?modes][&start][&count]
-resource.uri <- "Naded/matches?modes=arena"
-uri <- paste0(base.uri, resource.uri)
-response <- GET(uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
-response <- fromJSON(content(response, "text"))
-match.id <- response$Results
+rows.to.used <- seq(10, 169, 10)
+rows.to.used <- c(1, rows.to.used)
 
-#call to get the results from players in a specific match
-base.uri <- "https://www.haloapi.com/stats/h5/arena/matches/"
-#matchID
-resource.uri <- "ce9a15c4-a62e-4ab1-b5f3-af52871287c6"
-uri <- paste0(base.uri,resource.uri)
-response <- GET(uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
-response <- fromJSON(content(response, "text"))
-arena.results <- response$PlayerStats
+condense.leaders <- leaders[rows.to.used, ]
+gametags <- condense.leaders[["Player.Gamertag" ]]
+
+#This will add a '+' instead of a space in the URL for the 
+#GetMatchID function
+gametags <- gsub('([[:punct:]])|\\s+','+',gametags)
 
 
+GetMatchID <- function(tag) {
+  Sys.sleep(1)
+  base.uri <- paste0("https://www.haloapi.com/stats/h5/players/", tag, "/matches?modes=Arena")
+  response <- GET(base.uri, add_headers("Ocp-Apim-Subscription-Key" = api.key))
+  results <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
+  results <- results$Results
+  results <- as.data.frame(results)
+  results <- flatten(results)
+  return(results$Id.MatchId)
+}
+
+
+lapply(gametags,GetMatchID)
+#For Loop to create a dataframe of gamertags by matchID
+all <- lapply(gametags, GetMatchID)
+all.df <- all[[1]]
+for (i in 2:17) {
+  y <- all[[i]]
+  all.df <- rbind(all.df, y)
+}
+
+#putting the matchID per gamertag in a vector
+match.id.gametags <- c(all.df[1:17])
+
+#This function will get me the information for the number of kills and deaths
+#per player.
+GetMatchResults <- function(id) {
+  Sys.sleep(1)
+  base.uri <- paste0("https://www.haloapi.com/stats/h5/arena/matches/", id)
+  response <- GET(base.uri, add_headers("Ocp-Apim-Subscription-Key" = api.key))
+  results <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
+  results <- results$PlayerStats
+  results <- as.data.frame(results)
+  results <- flatten(results)
+  return(data.frame(GamerTag = results$Player.Gamertag,  
+                    TotalKills = results$TotalKills, 
+                    TotalDeaths = results$TotalDeaths,
+                    stringsAsFactors = FALSE))
+}
+
+#using lapply to get the match results based off of the ID from the top players
+lapply(match.id.gametags, GetMatchResults)
+
+#taking the results froma above and looping through to create a dataframe of the
+#information
+kd <- lapply(match.id.gametags, GetMatchResults)
+kd.df <- kd[[1]]
+for (i in 2:17) {
+  y <- kd[[i]]
+  kd.df <- rbind(kd.df, y)
+}
+
+#creating a column that has the kill death ratio per player
+kd.df$`Kill Death Ratio` <- (kd.df$TotalKills / kd.df$TotalDeaths)
 
 ###################################################
 ########## Defining the server ####################
 ###################################################
 my.server <- function(input, output) {
-
+  output$table <- renderTable({
+    player.selector <- subset(kd.df, 
+                               kd.df$GamerTag %in% input$player)
+  })
 }
 
 shinyServer(my.server)
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 
+
 # Define UI for application that draws a histogram
 my.ui <- fluidPage(
 
@@ -10,19 +11,32 @@ my.ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput('playlists',
-                         'Playlists:',
-                         1998:2014, selected = c(2000, 2010)),
-      sliderInput("range",
-                  "Range of ranked players: ",
-                  min = 0,
-                  max = 250,
-                  value = c(0, 100)),
-      width = 3
-    ),
+      
+      checkboxGroupInput('player', 'Select a Player:', choices = list ("Zeldun", "Respct", "Txrminator", "iTripnotic", "BBrandxn",   
+                                                                "Bizarrest", "DeaThLowKy", "Crewman XII", "iMariin", "xTheBoss Ryan",
+                                                                "Midnite Sol", "FRNCHFRlESATTAK", "FvG Youngblood", "Trivia o", 
+                                                                "DUD3 WheresMyAR", "Trevaholic", "OG Fractur3")
+      ),
+      
+      br()
 
     # create the different tabs plus text to go along with each tab
-    mainPanel()
-  )
-)
+   
+             ),
+    mainPanel(
+      tabsetPanel(type = "tabs",
+      tabPanel("muhammad", tableOutput("table"),
+               p("The information below displays a table of the top players in Halo
+                 who play the game mode called Arena. The information shows the Gamertag of
+                 the player which is a unique identifier per character. The next two columns
+                 have the total number of kills and deaths the player has in the match. Lastly,
+                 the last column is the Kill Death Ratio of the player in that match. The Kill
+                 Death Ratio per player in a match is important because having a high Kill Death
+                 Ratio will allow the character to receive certain rewards in the game.")
+            )
+          )
+        )
+      )
+    )
 shinyUI(my.ui)
+
