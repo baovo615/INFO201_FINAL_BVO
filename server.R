6968fbeb70cc88f27a29c6485e52c87882a83d1f
Muhammad Hussain <mnh78614@gmail.com>
2018-03-07 15:49:00 -0800

    converted the dataframe to a csv file so the information would load in faster...

--- server.R
+++ server.R
@@ -18,24 +18,7 @@ source("apikey.R")
 ###################################################
 ########## Process the data #######################
 ###################################################
-# to get a list of playlists that are ranked and active so we know
-# which ones to display to the reader
-base.uri <- "https://www.haloapi.com/metadata/h5/metadata/playlists"
-response <- GET(base.uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
-playlists <- fromJSON(content(response, "text"))
-playlists <- playlists %>%
-  filter(isRanked == TRUE & isActive == TRUE) %>%
-  select(name, description, gameMode, id)
-# team arena's id c98949ae-60a8-43dc-85d7-0feb0b92e719
 
-# to get the most recent finished season
-base.uri <- "https://www.haloapi.com/metadata/h5/metadata/seasons"
-response <- GET(base.uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
-seasons <- fromJSON(content(response, "text"))
-latest.finished.season.id <- seasons %>%
-  filter(isActive == FALSE) %>% filter(row_number()==n()) %>% select(id)
-latest.finished.season.id <- latest.finished.season.id[1, 1]
-# the most recent finished season id f7b4e7b4-8f70-431d-b604-d13cffff1114
 
 base.uri <- "https://www.haloapi.com/stats/h5/player-leaderboards/csr/"
 # resource {seasonId}/{playlistId}
@@ -56,60 +39,64 @@ gametags <- condense.leaders[["Player.Gamertag" ]]
 gametags <- gsub('([[:punct:]])|\\s+','+',gametags)
 
 
-GetMatchID <- function(tag) {
-  Sys.sleep(1)
-  base.uri <- paste0("https://www.haloapi.com/stats/h5/players/", tag, "/matches?modes=Arena")
-  response <- GET(base.uri, add_headers("Ocp-Apim-Subscription-Key" = api.key))
-  results <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
-  results <- results$Results
-  results <- as.data.frame(results)
-  results <- flatten(results)
-  return(results$Id.MatchId)
-}
+#GetMatchID <- function(tag) {
+ # Sys.sleep(1)
+  #base.uri <- paste0("https://www.haloapi.com/stats/h5/players/", tag, "/matches?modes=Arena")
+  #response <- GET(base.uri, add_headers("Ocp-Apim-Subscription-Key" = api.key))
+  #results <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
+  #results <- results$Results
+  #results <- as.data.frame(results)
+  #results <- flatten(results)
+  #return(results$Id.MatchId)
+#}
 
 
-lapply(gametags,GetMatchID)
+#lapply(gametags,GetMatchID)
 #For Loop to create a dataframe of gamertags by matchID
-all <- lapply(gametags, GetMatchID)
-all.df <- all[[1]]
-for (i in 2:17) {
-  y <- all[[i]]
-  all.df <- rbind(all.df, y)
-}
+#all <- lapply(gametags, GetMatchID)
+#all.df <- all[[1]]
+#for (i in 2:17) {
+  #y <- all[[i]]
+  #all.df <- rbind(all.df, y)
+#}
 
 #putting the matchID per gamertag in a vector
-match.id.gametags <- c(all.df[1:17])
+#match.id.gametags <- c(all.df[1:17])
 
 #This function will get me the information for the number of kills and deaths
 #per player.
-GetMatchResults <- function(id) {
-  Sys.sleep(1)
-  base.uri <- paste0("https://www.haloapi.com/stats/h5/arena/matches/", id)
-  response <- GET(base.uri, add_headers("Ocp-Apim-Subscription-Key" = api.key))
-  results <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
-  results <- results$PlayerStats
-  results <- as.data.frame(results)
-  results <- flatten(results)
-  return(data.frame(GamerTag = results$Player.Gamertag,  
-                    TotalKills = results$TotalKills, 
-                    TotalDeaths = results$TotalDeaths,
-                    stringsAsFactors = FALSE))
-}
+#GetMatchResults <- function(id) {
+  #Sys.sleep(1)
+  #base.uri <- paste0("https://www.haloapi.com/stats/h5/arena/matches/", id)
+  #response <- GET(base.uri, add_headers("Ocp-Apim-Subscription-Key" = api.key))
+  #results <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
+  #results <- results$PlayerStats
+  #results <- as.data.frame(results)
+  #results <- flatten(results)
+  #return(data.frame(GamerTag = results$Player.Gamertag,  
+                    #TotalKills = results$TotalKills, 
+                    #TotalDeaths = results$TotalDeaths,
+                    #stringsAsFactors = FALSE))
+#}
 
 #using lapply to get the match results based off of the ID from the top players
-lapply(match.id.gametags, GetMatchResults)
+#lapply(match.id.gametags, GetMatchResults)
 
 #taking the results froma above and looping through to create a dataframe of the
 #information
-kd <- lapply(match.id.gametags, GetMatchResults)
-kd.df <- kd[[1]]
-for (i in 2:17) {
-  y <- kd[[i]]
-  kd.df <- rbind(kd.df, y)
-}
+#kd <- lapply(match.id.gametags, GetMatchResults)
+#kd.df <- kd[[1]]
+#for (i in 2:17) {
+ # y <- kd[[i]]
+#  kd.df <- rbind(kd.df, y)
+#}
 
 #creating a column that has the kill death ratio per player
-kd.df$`Kill Death Ratio` <- (kd.df$TotalKills / kd.df$TotalDeaths)
+#kd.df$`Kill Death Ratio` <- (kd.df$TotalKills / kd.df$TotalDeaths)
+
+#average.kd <- round(mean(kd.df$`Kill Death Ratio`), digits = 2)
+write.csv(kd.df, "data/killdeathratio.csv")
+read.csv("data/killdeathratio.csv", stringsAsFactors = FALSE)
 
 ###################################################
 ########## Defining the server ####################
--- ui.R
+++ ui.R
@@ -12,10 +12,7 @@ my.ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       
-      checkboxGroupInput('player', 'Select a Player:', choices = list ("Zeldun", "Respct", "Txrminator", "iTripnotic", "BBrandxn",   
-                                                                "Bizarrest", "DeaThLowKy", "Crewman XII", "iMariin", "xTheBoss Ryan",
-                                                                "Midnite Sol", "FRNCHFRlESATTAK", "FvG Youngblood", "Trivia o", 
-                                                                "DUD3 WheresMyAR", "Trevaholic", "OG Fractur3")
+      checkboxGroupInput('player', 'Select a Player:', choices = c(kd.df$GamerTag)
       ),
       
       br()
@@ -26,13 +23,14 @@ my.ui <- fluidPage(
     mainPanel(
       tabsetPanel(type = "tabs",
       tabPanel("muhammad", tableOutput("table"),
-               p("The information below displays a table of the top players in Halo
+               p("The information above displays a table of the top players in Halo
                  who play the game mode called Arena. The information shows the Gamertag of
                  the player which is a unique identifier per character. The next two columns
                  have the total number of kills and deaths the player has in the match. Lastly,
                  the last column is the Kill Death Ratio of the player in that match. The Kill
                  Death Ratio per player in a match is important because having a high Kill Death
-                 Ratio will allow the character to receive certain rewards in the game.")
+                 Ratio will allow the character to receive certain rewards in the game. In
+                 this game mode, the average Kill Death Ratio is", strong(average.kd),".")
             )
           )
         )
