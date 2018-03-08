f2ee16833b8e13b9cfa8b2095869fc2e297cd412
luxizheng <lucyzheng09@gmail.com>
2018-03-07 18:18:18 -0800

    csv file

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ source("server.R")
 
 # Define UI for application that draws a histogram
 my.ui <- fluidPage(
-  
+  img(src='halo.jpeg', align = "right"),
   # Application title
   titlePanel("HALO 5"), 
   h4("Due to a limitation on the calls we can get from the API, please wait roughly 20 seconds for this page to get the most updated Halo data."),
@@ -16,11 +16,11 @@ my.ui <- fluidPage(
              p("The dataset that we will be working with is the", a("HALO 5 API", href="https://developer.haloapi.com"), ". The documentation for this API was created by 343 Industries, the studio that has been making the Halo Games since Halo 4. The dataset that we’re looking at is related to player and match information in matches. Some of the information that is included in the dataset are the results of a match, history of matches per player, match leaderboards, and player service records. It is recommended to have some prior knowledge, such as knowledge of first person shooter games, how online multiplayer FPS games function, the different game modes in the game, etc before viewing this dataset."),
              br(),
              h4("AUDIENCE"),
-             p("Our target audience is mainly Halo players or Halo developers."),
+             p("Our target audience is mainly Halo players."),
              br(),
              h4("QUESTIONS"),
              tags$li("What matches do players tend to do best in?"),
-             tags$li("How do different service records relate to each other? (ex: experience points vs. total kills"), 
+             tags$li("How do different service records relate to each other? (ex: experience points vs. total kills)"), 
              tags$li("How are the number of hours played on different playlists distributed for top ranked players?"),
              br(),
              h4("CREATED BY"),
@@ -41,7 +41,7 @@ my.ui <- fluidPage(
                  tableOutput("stats"),
                  br(),
                  p("This following is a table with descriptions for each playlist."),
-                 tableOutput("description")
+                 tableOutput("description")s
                )
              )
     ),
