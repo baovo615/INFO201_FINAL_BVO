ecc2423198f667ce773c407b2532246627831e0c
luxizheng <lucyzheng09@gmail.com>
2018-03-07 16:04:01 -0800

    saved into csv files + some edits

--- server.R
+++ server.R
@@ -1,7 +1,6 @@
 # Luxi Zheng
 
 library("shiny")
-
 library("rlang")
 library("httr")
 # install.packages("tidyr")
@@ -18,67 +17,76 @@ source("apikey.R")
 
 ###################################################
 ########## Process the data #######################
+# the first part of processing the data is commented out becuase 
+# I have to get each player's match history and at a limit of 10 calls per 10 second,
+# I had to save the data into a csv file and then use that csv file for the shiny app
+# otherwise, the app would take 3 min to run to display the data
 ###################################################
+
 # to get a list of playlists that are ranked and active so we know
 # which ones to display to the reader
 base.uri <- "https://www.haloapi.com/metadata/h5/metadata/playlists"
 response <- GET(base.uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
 playlists <- fromJSON(content(response, "text"))
-allplaylists <- playlists %>% select(name, description, gameMode, id)
-playlists <- playlists %>%
+allplaylists <- playlists %>% select(name, description, isRanked, isActive, gameMode, id)
+ranked.active.playlists <- playlists %>%
   filter(isRanked == TRUE & isActive == TRUE) %>%
   select(name, description, gameMode, id)
 # team arena's id c98949ae-60a8-43dc-85d7-0feb0b92e719
+# 
+# # to get the most recent finished season
+# base.uri <- "https://www.haloapi.com/metadata/h5/metadata/seasons"
+# response <- GET(base.uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
+# seasons <- fromJSON(content(response, "text"))
+# latest.finished.season.id <- seasons %>%
+#   filter(isActive == FALSE) %>% filter(row_number()==n()) %>% select(id)
+# latest.finished.season.id <- latest.finished.season.id[1, 1]
+# # the most recent finished season id f7b4e7b4-8f70-431d-b604-d13cffff1114
+# 
+# base.uri <- "https://www.haloapi.com/stats/h5/player-leaderboards/csr/"
+# # resource {seasonId}/{playlistId}
+# resource.uri <- paste0(latest.finished.season.id, "/c98949ae-60a8-43dc-85d7-0feb0b92e719?count=250")
+# uri <- paste0(base.uri, resource.uri)
+# response <- GET(uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
+# response <- fromJSON(content(response, "text"))
+# leaders <- response$Results
+# leaders <- flatten(leaders) 
+# 
+# rows.to.used <- seq(10, 169, 10)
+# rows.to.used <- c(1, rows.to.used) 
+# condense.leaders <- leaders[rows.to.used, ] 
+# 
+# allgametags <- leaders[['Player.Gamertag']]
+# allnospacetags <- gsub(" ", "+", allgametags)
+# 
+# GetMatches <- function(tag) {
+#   Sys.sleep(1)
+#   uri <- paste0("https://www.haloapi.com/stats/h5/players/", tag, "/matches?modes=Arena")
+#   response <- GET(uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
+#   results <- fromJSON(content(response, "text"))
+#   results <- results$Results
+#   results <- as.data.frame(results)
+#   results <- flatten(results)
+#   return(data.frame(PlaylistId = results$HopperId, MatchDuration = results$MatchDuration, stringsAsFactors = FALSE))
+# }
 
-# to get the most recent finished season
-base.uri <- "https://www.haloapi.com/metadata/h5/metadata/seasons"
-response <- GET(base.uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
-seasons <- fromJSON(content(response, "text"))
-latest.finished.season.id <- seasons %>%
-  filter(isActive == FALSE) %>% filter(row_number()==n()) %>% select(id)
-latest.finished.season.id <- latest.finished.season.id[1, 1]
-# the most recent finished season id f7b4e7b4-8f70-431d-b604-d13cffff1114
+# all <- lapply(allnospacetags, GetMatches)
 
-base.uri <- "https://www.haloapi.com/stats/h5/player-leaderboards/csr/"
-# resource {seasonId}/{playlistId}
-resource.uri <- paste0(latest.finished.season.id, "/c98949ae-60a8-43dc-85d7-0feb0b92e719?count=250")
-uri <- paste0(base.uri, resource.uri)
-response <- GET(uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
-response <- fromJSON(content(response, "text"))
-leaders <- response$Results
-leaders <- flatten(leaders) 
+# all.df <- all[[1]]
+# for (i in 2:169) {
+#   y <- all[[i]]
+#   all.df <- rbind(all.df, y)
+# } 
+# all.df <- all.df %>% mutate(durations = as.duration(MatchDuration))
+# 
+# write.csv(all.df, "data/matches.csv")
 
-rows.to.used <- seq(10, 169, 10)
-rows.to.used <- c(1, rows.to.used) 
-condense.leaders <- leaders[rows.to.used, ] 
-gametags <- condense.leaders[['Player.Gamertag']]
-nospacetags <- gsub(" ", "+", gametags)
+data <- read.csv("data/matches.csv")
 
-GetMatches <- function(tag) {
-  Sys.sleep(1)
-  uri <- paste0("https://www.haloapi.com/stats/h5/players/", tag, "/matches?modes=Arena")
-  response <- GET(uri, add_headers('Ocp-Apim-Subscription-Key' = api.key))
-  results <- fromJSON(content(response, "text"))
-  results <- results$Results
-  results <- flatten(results)
-  return(data.frame(PlaylistId = results$HopperId, MatchDuration = results$MatchDuration, stringsAsFactors = FALSE))
-}
+total.by.playlist <- data %>% group_by(PlaylistId) %>% 
+  summarize(totalDuration = sum(as.numeric(durations)) /60 /60)
 
-all <- lapply(nospacetags, GetMatches)
-
-all.df <- all[[1]]
-for (i in 2:17) {
-  y <- all[[i]]
-  all.df <- rbind(all.df, y)
-}  
-all.df <- all.df %>% mutate(durations = as.duration(MatchDuration))
-
-total <- sum(all.df$durations)
-
-total.by.playlist <- all.df %>% group_by(PlaylistId) %>% summarize(totalDuration = sum(durations))
-
-playlists.with.duration <- left_join(allplaylists, total.by.playlist, by= c("id" = "PlaylistId")) %>% 
-  filter(!is.na(totalDuration)) 
+playlists.with.duration <- left_join(allplaylists, total.by.playlist, by= c("id" = "PlaylistId")) %>% filter(!is.na(totalDuration)) 
 
 ###################################################
 ########## Defining the server ####################
@@ -86,12 +94,23 @@ playlists.with.duration <- left_join(allplaylists, total.by.playlist, by= c("id"
 my.server <- function(input, output) {
   
   output$plot <- renderPlot({ 
-    hist <- barplot(playlists.with.duration$totalDuration, names.arg=playlists.with.duration$name, las=2, xlab = "Playlists", ylab = "Total Duration (s)")
+    info.to.use <- playlists.with.duration %>% filter(name %in% input$playlists)
+    par(mar=c(11, 5, 0.5, 0.5)) 
+    hist <- barplot(info.to.use$totalDuration, names.arg=info.to.use$name, las=2, ylab = "Total Number of Hours Played") 
     return(hist)
   })
   
+  # create a table with playlist information for those who don't know much about the game
   output$description <- renderTable({
-    result <- playlists.with.duration %>% select(name, description)
+    result <- playlists.with.duration %>% select(name, description, isRanked, isActive, gameMode)
+    return(result)
+  })
+  
+  output$stats <- renderTable({
+    result <- playlists.with.duration %>% filter(name %in% input$playlists)
+    totalhours <- sum(as.numeric(result$totalDuration))
+    result <- result %>% mutate(percentage = round(totalDuration / totalhours * 100))
+    result <- result %>% select(name, description, isRanked, isActive, gameMode, totalDuration, percentage)
     return(result)
   })
 }
